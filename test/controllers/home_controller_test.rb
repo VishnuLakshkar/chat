ce3c0c93d3f6acb0c5f1c2d17c3014cf6e435611
VishnuLakshkar <vishnulakshkar@gmail.com>
2019-05-28 14:42:56 +0530

    test cases

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -1,9 +1,28 @@
 require 'test_helper'
 
 class HomeControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get home_index_url
-    assert_response :success
+  def setup
+  	@user = users(:vishnu)
   end
 
+  test "should redirect to sign in when not login" do
+    get home_index_path
+    assert_redirected_to new_user_session_path 
+  end
+
+  test "should get sign up path" do
+  	get new_user_registration_path
+  	assert_response :success
+
+  	assert_select "a[href=?]", new_user_session_path
+  	assert_select "a[href=?]", new_user_registration_path
+	end
+
+	test "should get sign in path" do
+		get new_user_session_path
+		assert_response :success
+
+		assert_select "a[href=?]", new_user_session_path
+  	assert_select "a[href=?]", new_user_registration_path
+	end
 end
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,7 +1,48 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+	include Devise::Test::IntegrationHelpers
+  def setup
+  	@user = users(:vishnu)
+  end
+
+  test "user should login" do 
+  	sign_in users(:vishnu)
+  	get root_path
+  	assert_response :success
+
+  	assert_select "a[href=?]", users_path
+  	assert_select "a[href=?]", destroy_user_session_path
+  	assert_select "a[href=?]", home_index_path
+  	assert_select "a[href=?]", edit_user_registration_path
+  	assert_select "a[href=?]", block_list_user_path(@user.id)
+  	assert_select "a[href=?]", new_user_session_path, count: 0
+  	assert_select "a[href=?]", new_user_registration_path, count: 0
+  end
+
+  test "user should redirect to sign in if not logged in" do 
+  	get users_path
+  	assert_redirected_to new_user_session_path
+  	assert_select "a[href=?]", destroy_user_session_path, count: 0
+  	assert_select "a[href=?]", users_path, count: 0
+  	assert_select "a[href=?]", destroy_user_session_path, count: 0
+  	assert_select "a[href=?]", home_index_path, count: 0
+  	assert_select "a[href=?]", edit_user_registration_path, count: 0
+  	assert_select "a[href=?]", block_list_user_path(@user.id), count: 0
+	end
+
+	test "user should redirect to users path when log in" do 
+  	sign_in users(:vishnu)
+		get users_path
+		assert_response :success
+	end
+
+	test "user should redirect to edit" do 
+  	sign_in users(:vishnu)
+		get edit_user_registration_path
+		assert_response :success
+
+		assert_select "label", "Password"
+		assert_select "h3", "Cancel my account"
+	end
 end
