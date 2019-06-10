54a48f61d6ee710a00fde21a0aca3f6e582798f8
VishnuLakshkar <vishnulakshkar@gmail.com>
2019-06-10 15:11:58 +0530

    Pagination and refactoring

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,6 @@
 class HomeController < ApplicationController
   def index
   	session[:conversations] ||= []
-
     @users = User.where.not(id: current_user).paginate(page: params[:page], per_page: 5)
     @conversations = Conversation.includes(:recipient, :messages)
                                  .find(session[:conversations])
